284759b2010a1f0330a60ca5aed6866ef7fc0ac4
Akhil-star <akhilakki1305@gmail.com>
2020-10-07 00:11:39 +0530

    One special Charcter

--- src/test/java/com/cg/junit/UserRegistrationTest.java
+++ src/test/java/com/cg/junit/UserRegistrationTest.java
@@ -9,9 +9,9 @@ public class UserRegistrationTest {
     @Test
     public void givenPassword_whenMatches_ShouldReturn() {
         UserRegistration userRegistration = new UserRegistration();
-        String  password1 = userRegistration.password("AkhilKumar1");
-        String  password2 = userRegistration.password("Akhilkumar");
-        String regex = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+        String  password1 = userRegistration.password("AkhilKuma@3");
+        String  password2 = userRegistration.password("Akhilkumar@@4");
+        String regex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s)[a-zA-Z0-9]*[^a-zA-Z0-9\\s]{1}[a-zA-Z0-9]*$";
         Pattern p =Pattern.compile(regex);
         Assert.assertEquals(true , password1.matches( p.pattern() ));
         Assert.assertEquals(false,password2.matches( p.pattern() ));
