d27a1bc16396b1688b02552e4b133f10d12a9e02
Akhil-star <akhilakki1305@gmail.com>
2020-10-07 00:01:15 +0530

    Numeric atleast one

--- src/test/java/com/cg/junit/UserRegistrationTest.java
+++ src/test/java/com/cg/junit/UserRegistrationTest.java
@@ -9,9 +9,9 @@ public class UserRegistrationTest {
     @Test
     public void givenPassword_whenMatches_ShouldReturn() {
         UserRegistration userRegistration = new UserRegistration();
-        String  password1 = userRegistration.password("AkhilKumar");
-        String  password2 = userRegistration.password("akhil");
-        String regex = "^(?=.*[A-Z]).{8,}$";
+        String  password1 = userRegistration.password("AkhilKumar1");
+        String  password2 = userRegistration.password("Akhilkumar");
+        String regex = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
         Pattern p =Pattern.compile(regex);
         Assert.assertEquals(true , password1.matches( p.pattern() ));
         Assert.assertEquals(false,password2.matches( p.pattern() ));
