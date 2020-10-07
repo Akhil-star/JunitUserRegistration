2ad9ed03879a610a85f6b87fa0a33a898de32df6
Akhil-star <akhilakki1305@gmail.com>
2020-10-07 20:36:13 +0530

    all cases for user registration

--- src/main/java/com/cg/junit/UserRegistration.java
+++ src/main/java/com/cg/junit/UserRegistration.java
@@ -1,21 +1,72 @@
 package com.cg.junit;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
-    public String firstName(String firstName) {
-        return firstName;
-    }
-    public String emailId(String emailid) {
-        return emailid;
-    }
-    public String lastName(String lastname) {
-        return lastname;
+    public static boolean name(String name) {
+        String regex = "^[A-Z][a-zA-Z]{2,}$";
+        Pattern p = Pattern.compile( regex );
+        if (name == null) {
+            return false;
+        }
+        Matcher m = p.matcher( name );
+        return m.matches();
     }
 
-    public String phoneNo(String phoneno) {
-        return phoneno;
+    public static boolean phNo(String num) {
+        String regex = "^[0-9]{2}\\s[0-9]{10}$";
+        Pattern p = Pattern.compile( regex );
+        if (num == null) {
+            return false;
+        }
+        Matcher m = p.matcher( num );
+        return m.matches();
     }
-    public String password(String password) {
-        return password;
+
+    public static boolean emailId(String email) {
+        String regex = "^[A-Za-z0-9]{1}[A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9]+(\\.[A-Za-z0-9]+){0,1}(\\.[A-Za-z]{2,})$";
+        Pattern p = Pattern.compile( regex );
+        if (email == null) {
+            return false;
+        }
+        Matcher m = p.matcher( email );
+        return m.matches();
+    }
+
+    public static boolean password(String pass) {
+        String regex = "^(?=.*[0-9])" + "(?=.*[a-z])(?=.*[A-Z])" + "(?!.*[<>`])" + "(?=[^@#$%^&+=]*[@#$%^&+=][^@#$%^&+=]*$)" + ".{8,}$";
+        Pattern p = Pattern.compile( regex );
+        if (pass == null) {
+            return false;
+        }
+        Matcher m = p.matcher( pass );
+        return m.matches();
+    }
+
+    public String happySad(String message) {
+        if (message.contains( "sad" ))
+            return "sad";
+        else
+            return "happy";
+    }
+
+    public static void main(String[] args) {
+        System.out.println( "Welcome to User Registration" );
+        Scanner sc = new Scanner( System.in );
+        UserRegistration userRegistration = new UserRegistration();
+        System.out.println( "Enter First name " );
+        System.out.println( userRegistration.name( sc.nextLine() ) );
+        System.out.println( "Enter Last name " );
+        System.out.println( userRegistration.name( sc.nextLine() ) );
+        System.out.println( "Enter Phone number " );
+        System.out.println( userRegistration.phNo( sc.nextLine() ) );
+        System.out.println( "Enter the EmailId" );
+        System.out.println( userRegistration.emailId( sc.nextLine() ) );
+        System.out.println( "Enter Password " );
+        System.out.println( userRegistration.password( sc.nextLine() ) );
+        System.out.println( userRegistration.happySad( sc.nextLine() ) );
     }
 }
--- src/test/java/com/cg/junit/UserRegistrationTest.java
+++ src/test/java/com/cg/junit/UserRegistrationTest.java
@@ -7,61 +7,38 @@ import java.util.regex.Pattern;
 
 public class UserRegistrationTest {
 
-    @Test
-    public void givenFirstName_whenMatches_ShouldReturnTrue() {
-        UserRegistration userRegistration = new UserRegistration();
-        String firstname = userRegistration.firstName("Akhil");
-        String  firstname1 = userRegistration.firstName("ak");
-        String regex = "^[A-Z]"+"[A-Za-z]{2,}$";
-        Pattern p =Pattern.compile(regex);
-        Assert.assertEquals(true , firstname.matches( p.pattern() ));
-        Assert.assertEquals(false, firstname1.matches( p.pattern() ));
+    UserRegistration userRegistration = new UserRegistration();
 
+    @Test
+    public void givenFirstName_whenEntered_shouldReturnTrue() {
+        Assert.assertTrue( userRegistration.name( "Akhil" ) );
     }
 
     @Test
-    public void givenLastName_whenMatches_ShouldReturnTrue() {
-        UserRegistration userRegistration = new UserRegistration();
-        String  lastname = userRegistration.lastName("Mayabrahma");
-        String  lastname1 = userRegistration.lastName("ak");
-        String regex = "^[A-Z]"+"[A-Za-z]{2,}$";
-        Pattern p =Pattern.compile(regex);
-        Assert.assertEquals(true , lastname.matches( p.pattern() ));
-        Assert.assertEquals(false,lastname1.matches( p.pattern() ));
-
+    public void givenLastName_whenEntered_shouldReturnTrue() {
+        Assert.assertTrue( userRegistration.name( "Kumar" ) );
     }
 
     @Test
-    public void givenEmail_whenMatches_ShouldReturnTrue() {
-        UserRegistration userRegistration = new UserRegistration();
-        String  emailId1 = userRegistration.emailId("mayabrahma@gmail.com.com");
-        String  emailId2 = userRegistration.emailId("akl123@gmail.a");
-        String regex = "^[_a-z0-9-\\+]+(\\.[_a-z0-9-]+)*@[a-z0-9]+(\\.[a-z0-9]+)*(\\.[a-z]{2,})$";
-        Pattern p =Pattern.compile(regex);
-        Assert.assertEquals(true , emailId1.matches( p.pattern() ));
-        Assert.assertEquals(false,emailId2.matches( p.pattern() ));
-
+    public void givenMobile_whenEntered_shouldReturnTrue() {
+        Assert.assertTrue( userRegistration.phNo( "91 2581475625" ) );
     }
-    @Test
-    public void givenPhno_whenMatches_ShouldReturn() {
-        UserRegistration userRegistration = new UserRegistration();
-        String  phoneNo1 = userRegistration.phoneNo("91 8667772345");
-        String  phoneNo2 = userRegistration.phoneNo("9 9876556789");
-        String regex = "^[0-9]{2}\\s[0-9]{10}$";
-        Pattern p =Pattern.compile(regex);
-        Assert.assertEquals(true , phoneNo1.matches( p.pattern() ));
-        Assert.assertEquals(false,phoneNo2.matches( p.pattern() ));
 
+    @Test
+    public void givenPassword_whenEntered_shouldReturnTrue() {
+        Assert.assertTrue( userRegistration.password( "Akhilakki@2345" ) );
     }
-    @Test
-    public void givenPassword_whenMatches_ShouldReturn() {
-        UserRegistration userRegistration = new UserRegistration();
-        String  password1 = userRegistration.password("AkhilKuma@3");
-        String  password2 = userRegistration.password("Akhilkumar@@4");
-        String regex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s)[a-zA-Z0-9]*[^a-zA-Z0-9\\s]{1}[a-zA-Z0-9]*$";
-        Pattern p =Pattern.compile(regex);
-        Assert.assertEquals(true , password1.matches( p.pattern() ));
-        Assert.assertEquals(false,password2.matches( p.pattern() ));
 
+    @Test
+    public void givenString_whenEntered_shouldReturnSad() {
+        Assert.assertEquals( "happy", userRegistration.happySad( "happy" ) );
+    }
+
+
+    @Test
+    public void givenEmail_whenEntered_shouldReturnCorrect() {
+        Assert.assertTrue(  userRegistration.emailId( "akhilakki@gmail.com" ) );
     }
 }
+
+
