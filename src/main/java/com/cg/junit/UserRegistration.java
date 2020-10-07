970eaef9e6484420ecc16aec5d6be516e3322d41
Akhil-star <akhilakki1305@gmail.com>
2020-10-07 19:37:39 +0530

    First name test case

--- src/main/java/com/cg/junit/UserRegistration.java
+++ src/main/java/com/cg/junit/UserRegistration.java
@@ -1,8 +1,25 @@
 package com.cg.junit;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
-    public String firstName(String firstname) {
-        return firstname;
+    public static boolean name(String name) {
+        String regex = "^[A-Z][a-zA-Z]{2,}$";
+        Pattern p = Pattern.compile(regex);
+        if (name == null) {
+            return false;
+        }
+        Matcher m = p.matcher(name);
+        return m.matches();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        UserRegistration userRegistration = new UserRegistration();
+        System.out.println("Enter First name ");
+        System.out.println(userRegistration.name(sc.nextLine()));
     }
 }
--- src/test/java/com/cg/junit/UserRegistrationTest.java
+++ src/test/java/com/cg/junit/UserRegistrationTest.java
@@ -7,14 +7,9 @@ import java.util.regex.Pattern;
 
 public class UserRegistrationTest {
     @Test
-    public void givenFirstName_whenMatches_ShouldReturnTrue() {
+    public void givenFirstName_whenEntered_shouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        String  firstname = userRegistration.firstName("Akhil");
-        String firstname1 = userRegistration.firstName("akki");
-        String regex = "^[A-Z]"+"[A-Za-z]{2,}$";
-        Pattern p =Pattern.compile(regex);
-        Assert.assertEquals(true , firstname.matches( p.pattern() ));
-        Assert.assertEquals(false,firstname1.matches( p.pattern() ));
-
+        Assert.assertTrue( userRegistration.name( "Akhil" ) );
     }
+
 }
