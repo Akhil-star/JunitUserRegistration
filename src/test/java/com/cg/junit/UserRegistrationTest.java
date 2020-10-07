238afe0578356661016b5abbe084100cce3028ef
Akhil-star <akhilakki1305@gmail.com>
2020-10-07 19:43:21 +0530

    last name test case

--- src/test/java/com/cg/junit/UserRegistrationTest.java
+++ src/test/java/com/cg/junit/UserRegistrationTest.java
@@ -6,15 +6,10 @@ import org.junit.Test;
 import java.util.regex.Pattern;
 
 public class UserRegistrationTest {
-    @Test
-    public void givenLastName_whenMatches_ShouldReturnTrue() {
-        UserRegistration userRegistration = new UserRegistration();
-        String  lastname = userRegistration.lastName("Mayabrahma");
-        String  lastname1 = userRegistration.lastName("ak");
-        String regex = "^[A-Z]"+"[A-Za-z]{2,}$";
-        Pattern p =Pattern.compile(regex);
-        Assert.assertEquals(true , lastname.matches( p.pattern() ));
-        Assert.assertEquals(false,lastname1.matches( p.pattern() ));
 
+    @Test
+    public void givenLastName_whenEntered_shouldReturnTrue() {
+        UserRegistration userRegistration=new UserRegistration();
+        Assert.assertTrue( userRegistration.name( "Kumar" ) );
     }
 }
