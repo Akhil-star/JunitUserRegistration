0e3eb93181f0db0eb60bc0f241134fc516f6458e
Akhil-star <akhilakki1305@gmail.com>
2020-10-07 20:18:12 +0530

    One Special charcter

--- src/main/java/com/cg/junit/UserRegistration.java
+++ src/main/java/com/cg/junit/UserRegistration.java
@@ -1,8 +1,25 @@
 package com.cg.junit;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
-    public String password(String password) {
-        return password;
+    public static boolean password(String pass) {
+        String regex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s)[a-zA-Z0-9]*[^a-zA-Z0-9\\s]{1}[a-zA-Z0-9]*$";
+        Pattern p = Pattern.compile( regex );
+        if (pass == null) {
+            return false;
+        }
+        Matcher m = p.matcher( pass );
+        return m.matches();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner( System.in );
+        UserRegistration userRegistration = new UserRegistration();
+        System.out.println( "Enter Password " );
+        System.out.println( userRegistration.password( sc.nextLine() ) );
     }
 }
--- src/test/java/com/cg/junit/UserRegistrationTest.java
+++ src/test/java/com/cg/junit/UserRegistrationTest.java
@@ -7,14 +7,8 @@ import java.util.regex.Pattern;
 
 public class UserRegistrationTest {
     @Test
-    public void givenPassword_whenMatches_ShouldReturn() {
-        UserRegistration userRegistration = new UserRegistration();
-        String  password1 = userRegistration.password("AkhilKuma@3");
-        String  password2 = userRegistration.password("Akhilkumar@@4");
-        String regex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s)[a-zA-Z0-9]*[^a-zA-Z0-9\\s]{1}[a-zA-Z0-9]*$";
-        Pattern p =Pattern.compile(regex);
-        Assert.assertEquals(true , password1.matches( p.pattern() ));
-        Assert.assertEquals(false,password2.matches( p.pattern() ));
-
+    public void givenPassword_whenEntered_shouldReturnTrue() {
+        UserRegistration userRegistration=new UserRegistration();
+        Assert.assertTrue( userRegistration.password( "Akhilakki@2345" ) );
     }
 }
