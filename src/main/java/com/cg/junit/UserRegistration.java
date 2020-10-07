5664f2d6a45d7671d8ced591e81a43334bb05474
Akhil-star <akhilakki1305@gmail.com>
2020-10-07 20:04:49 +0530

    Minimum charcters test case

--- src/main/java/com/cg/junit/UserRegistration.java
+++ src/main/java/com/cg/junit/UserRegistration.java
@@ -1,8 +1,24 @@
 package com.cg.junit;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
-    public String password(String password) {
-        return password;
+    public static boolean password(String pass){
+        String regex =   "^(?=^.).{8,}$";
+        Pattern p = Pattern.compile(regex);
+        if (pass == null) {
+            return false;
+        }
+        Matcher m = p.matcher(pass);
+        return m.matches();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner( System.in );
+        UserRegistration userRegistration = new UserRegistration();
+        System.out.println( "Enter Password " );
+        System.out.println( userRegistration.password( sc.nextLine() ) );
     }
 }
--- src/test/java/com/cg/junit/UserRegistrationTest.java
+++ src/test/java/com/cg/junit/UserRegistrationTest.java
@@ -6,15 +6,10 @@ import org.junit.Test;
 import java.util.regex.Pattern;
 
 public class UserRegistrationTest {
-    @Test
-    public void givenPassword_whenMatches_ShouldReturn() {
-        UserRegistration userRegistration = new UserRegistration();
-        String  password1 = userRegistration.password("Akhilkumar@");
-        String  password2 = userRegistration.password("Akhil");
-        String regex = "^(?=^.).{8,}$";
-        Pattern p =Pattern.compile(regex);
-        Assert.assertEquals(true , password1.matches( p.pattern() ));
-        Assert.assertEquals(false,password2.matches( p.pattern() ));
 
-    }
+        @Test
+        public void givenPassword_whenEntered_shouldReturnTrue() {
+            UserRegistration userRegistration = new UserRegistration();
+            Assert.assertTrue( userRegistration.password( "Akhilakki@2345" ) );
+        }
 }
