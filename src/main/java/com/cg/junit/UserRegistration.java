4e260bfa0ddc54782dc47b6817336328df86054b
Akhil-star <akhilakki1305@gmail.com>
2020-10-07 19:58:42 +0530

    Phone number test case

--- src/main/java/com/cg/junit/UserRegistration.java
+++ src/main/java/com/cg/junit/UserRegistration.java
@@ -1,8 +1,25 @@
 package com.cg.junit;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
-    public String phoneNo(String phoneno) {
-        return phoneno;
+    public static boolean phNo(String num) {
+        String regex = "^[0-9]{2}\\s[0-9]{10}$";
+        Pattern p = Pattern.compile(regex);
+        if (num == null) {
+            return false;
+        }
+        Matcher m = p.matcher(num);
+        return m.matches();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner( System.in );
+        UserRegistration userRegistration = new UserRegistration();
+        System.out.println( "Enter Phone number " );
+        System.out.println( userRegistration.phNo( sc.nextLine() ) );
     }
 }
--- src/test/java/com/cg/junit/UserRegistrationTest.java
+++ src/test/java/com/cg/junit/UserRegistrationTest.java
@@ -6,15 +6,10 @@ import org.junit.Test;
 import java.util.regex.Pattern;
 
 public class UserRegistrationTest {
-    @Test
-    public void givenPhno_whenMatches_ShouldReturn() {
-        UserRegistration userRegistration = new UserRegistration();
-        String  phoneNo1 = userRegistration.phoneNo("91 8667772345");
-        String  phoneNo2 = userRegistration.phoneNo("9 9876556789");
-        String regex = "^[0-9]{2}\\s[0-9]{10}$";
-        Pattern p =Pattern.compile(regex);
-        Assert.assertEquals(true , phoneNo1.matches( p.pattern() ));
-        Assert.assertEquals(false,phoneNo2.matches( p.pattern() ));
 
+    @Test
+    public void givenMobile_whenEntered_shouldReturnTrue() {
+        UserRegistration userRegistration=new UserRegistration();
+        Assert.assertTrue( userRegistration.phNo( "91 2581475625" ) );
     }
 }
