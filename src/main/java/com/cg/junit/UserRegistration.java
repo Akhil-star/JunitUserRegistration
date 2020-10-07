6f15b40960540b06ddef1bf5f38a0c4a3c9ebb24
Akhil-star <akhilakki1305@gmail.com>
2020-10-07 20:27:06 +0530

    test case satisfies all emailids

--- src/main/java/com/cg/junit/UserRegistration.java
+++ src/main/java/com/cg/junit/UserRegistration.java
@@ -1,21 +1,25 @@
 package com.cg.junit;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
-    public String firstName(String firstName) {
-        return firstName;
-    }
-    public String emailId(String emailid) {
-        return emailid;
-    }
-    public String lastName(String lastname) {
-        return lastname;
+    public static boolean emailId(String email) {
+        String regex = "^[A-Za-z0-9]{1}[A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9]+(\\.[A-Za-z0-9]+){0,1}(\\.[A-Za-z]{2,})$";
+        Pattern p = Pattern.compile(regex);
+        if (email == null) {
+            return false;
+        }
+        Matcher m = p.matcher(email);
+        return m.matches();
     }
 
-    public String phoneNo(String phoneno) {
-        return phoneno;
-    }
-    public String password(String password) {
-        return password;
+    public static void main(String[] args) {
+        Scanner sc=new Scanner( System.in );
+        UserRegistration userRegistration=new UserRegistration();
+        System.out.println("Enter the EmailId");
+        System.out.println(userRegistration.emailId(sc.nextLine()));
     }
 }
--- src/test/java/com/cg/junit/UserRegistrationTest.java
+++ src/test/java/com/cg/junit/UserRegistrationTest.java
@@ -6,17 +6,11 @@ import org.junit.Test;
 import java.util.regex.Pattern;
 
 public class UserRegistrationTest {
-    
-    @Test
-    public void givenEmail_whenMatches_ShouldReturnTrue() {
-        UserRegistration userRegistration = new UserRegistration();
-        String  emailId1 = userRegistration.emailId("mayabrahma@gmail.com.com");
-        String  emailId2 = userRegistration.emailId("akl123@gmail.a");
-        String regex = "^[_a-z0-9-\\+]+(\\.[_a-z0-9-]+)*@[a-z0-9]+(\\.[a-z0-9]+)*(\\.[a-z]{2,})$";
-        Pattern p =Pattern.compile(regex);
-        Assert.assertEquals(true , emailId1.matches( p.pattern() ));
-        Assert.assertEquals(false,emailId2.matches( p.pattern() ));
 
+    @Test
+    public void givenEmail_whenEntered_shouldReturnCorrect() {
+        UserRegistration userRegistration=new UserRegistration();
+        Assert.assertTrue( userRegistration.emailId( "akhilakki13@gmail.com" ) );
     }
 
 }
