92d557d89ebe8b44fa16457eaa00b5e80bf09be8
Akhil-star <akhilakki1305@gmail.com>
2020-10-09 12:40:24 +0530

    Only one special char

--- src/main/java/com/cg/junit/UserRegistration.java
+++ src/main/java/com/cg/junit/UserRegistration.java
@@ -6,12 +6,29 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
+    public static boolean name(String name) {
+        String regex = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        return  pattern.matcher(name).matches();
+    }
+
+    public static boolean emailId(String email) {
+        String regex = "^[A-Za-z0-9]{1}[A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9]+(\\.[A-Za-z0-9]+){0,1}(\\.[A-Za-z]{2,})$";
+        Pattern p = Pattern.compile( regex );
+        Matcher m = p.matcher( email );
+        return m.matches();
+    }
+
+    public static boolean phNo(String num) {
+        String regex = "^[0-9]{2}\\s[0-9]{10}$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(num);
+        return m.matches();
+    }
+
     public static boolean password(String pass) {
         String regex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s)[a-zA-Z0-9]*[^a-zA-Z0-9\\s]{1}[a-zA-Z0-9]*$";
         Pattern p = Pattern.compile( regex );
-        if (pass == null) {
-            return false;
-        }
         Matcher m = p.matcher( pass );
         return m.matches();
     }
@@ -19,6 +36,14 @@ public class UserRegistration {
     public static void main(String[] args) {
         Scanner sc = new Scanner( System.in );
         UserRegistration userRegistration = new UserRegistration();
+        System.out.println("Enter the First name");
+        System.out.println(userRegistration.name(sc.nextLine()));
+        System.out.println("Enter the last name");
+        System.out.println(userRegistration.name(sc.nextLine()));
+        System.out.println("Enter the EmailId");
+        System.out.println(userRegistration.emailId(sc.nextLine()));
+        System.out.println( "Enter Phone number " );
+        System.out.println( userRegistration.phNo( sc.nextLine() ) );
         System.out.println( "Enter Password " );
         System.out.println( userRegistration.password( sc.nextLine() ) );
     }
