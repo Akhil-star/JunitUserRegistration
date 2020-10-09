4dc7b9fbfed98c1a2313513f0a0d993d7f2528b0
Akhil-star <akhilakki1305@gmail.com>
2020-10-09 11:32:40 +0530

    Fisrt name test cases

--- src/main/java/com/cg/junit/UserRegistration.java
+++ src/main/java/com/cg/junit/UserRegistration.java
@@ -6,20 +6,16 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    public static boolean name(String name) {
-        String regex = "^[A-Z][a-zA-Z]{2,}$";
-        Pattern p = Pattern.compile(regex);
-        if (name == null) {
-            return false;
-        }
-        Matcher m = p.matcher(name);
-        return m.matches();
+    public static boolean firstName(String fname) {
+        String regex = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        return  pattern.matcher(fname).matches();
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         UserRegistration userRegistration = new UserRegistration();
         System.out.println("Enter First name ");
-        System.out.println(userRegistration.name(sc.nextLine()));
+        System.out.println(userRegistration.firstName(sc.nextLine()));
     }
 }
--- src/test/java/com/cg/junit/UserRegistrationTest.java
+++ src/test/java/com/cg/junit/UserRegistrationTest.java
@@ -7,9 +7,23 @@ import java.util.regex.Pattern;
 
 public class UserRegistrationTest {
     @Test
-    public void givenFirstName_whenEntered_shouldReturnTrue() {
+    public void givenFirstName_whenProper_shouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        Assert.assertTrue( userRegistration.name( "Akhil" ) );
+        boolean result =userRegistration.firstName("Akhil");
+        Assert.assertTrue(result );
     }
+    @Test
+    public void givenFirstName_whenNotProper_shouldReturnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result =userRegistration.firstName("Ak");
+        Assert.assertFalse( result );
+    }
+    @Test
+    public void givenFirstName_whenSpecialChar_shouldReturnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result =userRegistration.firstName("Akhil@");
+        Assert.assertFalse(result );
+    }
+
 
 }
